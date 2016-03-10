658e1daa1ad7355a0ace59533c722733687d2597
Andres Gomez Urquiza <andres@fractal.ai>
2016-03-10 14:33:15 -0600

    fix

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install ros-hydro-desktop-full
+sudo apt-get install -y ros-hydro-desktop-full
 sudo rosdep init
 rosdep update
 
 echo "source /opt/ros/hydro/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install python-rosinstall
+sudo apt-get install -y python-rosinstall
 
-sudo apt-get install ros-hydro-industrial-desktop
+sudo apt-get install -y ros-hydro-industrial-desktop
