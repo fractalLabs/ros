76fc53f0320dc9ec6855831ae108ab1b82a4938a
Andres Gomez Urquiza <andres@fractal.ai>
2016-03-10 14:38:54 -0600

    also upgrades all packages

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
+sudo apt-get upgrade -y
 sudo apt-get install -y ros-hydro-desktop-full
 sudo rosdep init
 rosdep update
